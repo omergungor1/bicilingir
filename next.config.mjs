760dca8b3d769461dc7c914fab56f2aa80893dca
Ömer <omergungorco@gmail.com>
2025-03-16 16:34:32 +0300

    some impr

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Font optimizasyonunu devre dışı bırakalım
-  optimizeFonts: false,
   // Statik dosyaların yolunu düzeltelim
   poweredByHeader: false,
   reactStrictMode: true,
-  swcMinify: true,
   // CSS sorunlarını çözmek için ek yapılandırmalar
   experimental: {
     optimizeCss: {
@@ -32,4 +29,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default nextConfig; 
