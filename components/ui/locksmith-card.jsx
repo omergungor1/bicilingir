2b030d76418ecbf6ae096cba4bc4b6393ff4d319
Ömer <omergungorco@gmail.com>
2025-05-20 01:11:19 +0300

    speed test 3

--- components/ui/locksmith-card.jsx
+++ components/ui/locksmith-card.jsx
@@ -364,10 +364,10 @@ export default function LocksmithCard({ locksmith, index }) {
                     <div className="space-y-3">
                         <a
                             href={`tel:${locksmith.phone.replace(/\D/g, '')}`}
-                            className="w-full flex items-center justify-center py-2 px-4 bg-[#4169E1] hover:bg-[#3658c7] text-white rounded-md shadow-md transition-colors gap-2"
+                            className="w-full flex items-center justify-center py-2 px-4 bg-[#4169E1] hover:bg-[#3658c7] text-white font-bold text-lg rounded-md shadow-md transition-colors gap-2"
                             onClick={() => handleCallLocksmith(locksmith, index)}
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                             </svg>
                             {locksmith.phone}
@@ -375,7 +375,7 @@ export default function LocksmithCard({ locksmith, index }) {
                         {/* Whatsapp Butonu */}
                         <Button
                             variant="outline"
-                            className={`w-full text-white flex items-center justify-center gap-2 bg-green-600 hover:bg-green-700 font-bold shadow-md`}
+                            className={`w-full text-white flex items-center justify-center gap-2 bg-green-600 hover:bg-green-700 font-bold shadow-md opacity-85`}
                             onClick={() => handleWhatsappMessage(locksmith, index)}
                         >
                             <svg
